919a12df3147afdc9132b52abdeb97f8cad61fe1
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2019-01-03 14:06:31 -0500

    init

---
+++ slacktweet.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+"""
+Slackbot Project
+"""
+
+__author__ = "Slackbot Team: Jake Johnson and Kyle Meiklejohn"
+
+
+def main():
+    """ Main entry point of the app """
+    pass
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
