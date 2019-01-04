2209b1f99b3f23ab249ba6e7f11c5532f9063957
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2019-01-03 21:38:14 -0500

    setup basic slackbot structure including environment variables and connected slack app.exit  messages and mentions are being monitored and logged.

--- settings.py
+++ settings.py
@@ -3,11 +3,7 @@
 
 from pathlib import Path  # python3 only
 from dotenv import load_dotenv
-import os
-
 
 # OR, explicitly providing path to '.env'
 from dotenv import load_dotenv
 load_dotenv()
-
-print(SECRET_KEY)
