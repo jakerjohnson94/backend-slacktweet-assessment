0d951368bb8fa25103274f937726237f9cd8c602
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2019-01-03 21:46:51 -0500

    removed settings.py as env variables are being run from command line

--- settings.py
+++
@@ -1,9 +0,0 @@
-# settings.py
-#! ./py3env/bin/python3.7
-
-from pathlib import Path  # python3 only
-from dotenv import load_dotenv
-
-# OR, explicitly providing path to '.env'
-from dotenv import load_dotenv
-load_dotenv()
